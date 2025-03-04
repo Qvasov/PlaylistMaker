6a00d9a10403783a228766aac1423ce6712d75ab
Qvasov <Qvasov@gmail.com>
2025-03-04 20:28:31 +0300

    Context and Intent

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -36,8 +36,6 @@ class MainActivity : AppCompatActivity() {
             startActivity(settingsIntent)
         }
 
-
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,9 @@
 package com.practicum.playlistmaker
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.FrameLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +14,32 @@ class SettingsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
+
+        val shareButton = findViewById<FrameLayout>(R.id.share)
+        shareButton.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type = "text/plain"
+            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.course_url))
+            startActivity(Intent.createChooser(shareIntent, null))
+        }
+
+        val supportButton = findViewById<FrameLayout>(R.id.support)
+        supportButton.setOnClickListener {
+            val supportIntent = Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:"))
+
+            supportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.emailTo)))
+            supportIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.subject))
+            supportIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.text))
+            startActivity(supportIntent)
+        }
+
+        val userAgreementButton = findViewById<FrameLayout>(R.id.user_agreement)
+        userAgreementButton.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.user_agreement_url)))
+            startActivity(shareIntent)
+        }
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.settings)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
