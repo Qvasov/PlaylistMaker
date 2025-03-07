56ac5b9ad15baaa6e2ec7e3e0a08c830fe7f1883
Qvasov <Qvasov@gmail.com>
2025-03-07 23:23:06 +0300

    Sprint 10

---
+++ app/src/main/java/com/practicum/playlistmaker/searchview/TrackAdapter.kt
@@ -0,0 +1,48 @@
+package com.practicum.playlistmaker.searchview
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.practicum.playlistmaker.R
+
+class TrackAdapter(
+    private val trackList: List<Track>
+) : RecyclerView.Adapter<TrackAdapter.TrackHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.track_view, parent, false)
+        return TrackHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TrackHolder, position: Int) {
+        holder.bind(trackList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return trackList.size
+    }
+
+    class TrackHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val trackViewImage = itemView.findViewById<ImageView>(R.id.trackViewImage)
+        private val trackViewTextTrackName = itemView.findViewById<TextView>(R.id.trackViewTextTrackName)
+        private val trackViewTextArtisName = itemView.findViewById<TextView>(R.id.trackViewTextArtisName)
+        private val trackViewTextTrackTime = itemView.findViewById<TextView>(R.id.trackViewTextTrackTime)
+
+        fun bind(track: Track) {
+            Glide.with(itemView)
+                .load(track.artworkUrl100)
+                .placeholder(R.drawable.barsik)
+                .centerCrop()
+                .transform(RoundedCorners(2))
+                .into(trackViewImage)
+            trackViewTextTrackName.text = track.trackName
+            trackViewTextArtisName.text = track.artistName
+            trackViewTextTrackTime.text = track.trackTime
+        }
+    }
+}
